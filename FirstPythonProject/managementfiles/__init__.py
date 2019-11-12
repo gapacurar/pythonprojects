733af5f3fde21aac38d774f8bca99bf344e7320f
gapacurar <gheorghe.pacurar@a-sti.eu>
2019-11-12 13:14:36 +0200

    Adding Python management files package and module - Create, open and write content to files. Reading partial content. Delete created file.

--- FirstPythonProject/managementfiles/__init__.py
+++ FirstPythonProject/managementfiles/__init__.py
@@ -66,3 +66,9 @@ except:
 finally:
     myfiletoreadlines.close()
     print("This is block finally of reading line by line of file")
+# 
+import os
+if os.path.exists("myfile.txt"):
+  os.remove("myfile.txt")
+else:
+  print("The file does not exist")
